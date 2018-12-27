39d289464e40bf8044d59c99a795ce85deca1af9
mrajender <mrajender@mrajender-lp.pkrmsezdc.com>
2018-12-27 20:27:19 +0530

    naming conversion change

--- spring-file-demo/src/main/java/com/example/demo/util/ContentType.java
+++ spring-file-demo/src/main/java/com/example/demo/util/ContentType.java
@@ -2,16 +2,16 @@ package com.example.demo.util;
 
 import java.util.Arrays;
 
-public enum CONTENTTYPE {
+public enum ContentType {
 	PDF("pdf"), XLSX("xlsx"), XLS("xls"), DOC("doc"), DOCS("docx");
 
 	private String contentType;
 
-	CONTENTTYPE(String contentType) {
+	ContentType(String contentType) {
 		this.contentType = contentType;
 	}
 
 	public static boolean getFileType(String contentType) {
-		return Arrays.stream(CONTENTTYPE.values()).filter(c -> c.contentType.equals(contentType)).findFirst().isPresent();
+		return Arrays.stream(ContentType.values()).filter(c -> c.contentType.equals(contentType)).findFirst().isPresent();
 	}
 }
